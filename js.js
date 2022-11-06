6e846816a0b3d176ae0172bbadff64dce5da0c12
christinezhangg <54513295+christinezhangg@users.noreply.github.com>
2022-11-06 16:25:38 -0600

    still can't figure out left and right arrows!

--- js.js
+++ js.js
@@ -5,16 +5,27 @@ var images = document.getElementsByClassName("images");
 var imageId = 0;
 
 //bigimage was completed with a TA who directed me towards https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp for assistance.
-//she okayed everything that I completed here. 
+//Molly okayed everything that I completed here. 
 function bigimage(imgs) {
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
-  }
+}
 
-//
-right.addEventListener("click", moveright);
-    function moveright() {
+function moveleft() {
+    for (i = 0; i < images.length; ++i){
+        if (imageId == 0){
+            imageId = 11;
+            bigimage(imageId);
+        } else {
+            imageId -= 1;
+            bigimage(imageId);
+        }
+    }
+}
+
+function moveright() {
+    for (i = 0; i < images.length; ++i){
         if (imageId == 11){
             imageId = 0;
             bigimage(imageId);
@@ -23,20 +34,7 @@ right.addEventListener("click", moveright);
             bigimage(imageId);
         }
     }
-
-left.addEventListener("click", moveleft);
-    function moveright() {
-        if (imageId == 11){
-            imageId = 0;
-            bigimage(imageId);
-        } else {
-            imageId += 1;
-            bigimage(imageId);
-        }
-    }
-
-
-
+}
 
 
 
