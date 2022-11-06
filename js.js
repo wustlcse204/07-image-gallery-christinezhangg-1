47b6d62b064179573499206f27630cbcc9107e53
christinezhangg <54513295+christinezhangg@users.noreply.github.com>
2022-11-06 15:36:07 -0600

    big image and close completed... just need to complete arrow functions then done!

--- js.js
+++ js.js
@@ -4,38 +4,40 @@ var close = document.getElementById("x-mark");
 var images = document.getElementsByClassName("images");
 var imageId = 0;
 
-//First function for this assignment is to close the image that is currently open by hitting the x.
-close.addEventListener("click", closeimage);
-function closeimage() {
-    for (i = 0; i < images.length; ++i){
-        images[i].style.display = 'none';
-    }
-}
+//bigimage was completed with a TA who directed me towards https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp for assistance.
+//she okayed everything that I completed here. 
+function bigimage(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+  }
 
-//Second function for this assignment is to increase the size of the image that is clicked on. 
-for (i = 0; i < images.length; ++i){
-    images[i].addEventListener("click", bigimage);
-}
-function bigimage() {
-    if (i){
-        this.style.width = '200%';
-        this.style.height = 'auto';
-    } else {
-        this.style.display = "none";
-    }
-}
-
-//Third function will focus on the right arrow moving from picture to picture.
-//Simply just the Javascript, not implemented unlike the top two functions.  This is because I have not yet set up the final image structure.  
-//However, this exact Javascript function will be used in my final submission once everything else is set up.  
+//
 right.addEventListener("click", moveright);
-function moveright() {
-    if (imageId == 11){
-        imageId = 0;
-    } else {
-        imageId += 1;
+    function moveright() {
+        if (imageId == 11){
+            imageId = 0;
+            bigimage(imageId);
+        } else {
+            imageId += 1;
+            bigimage(imageId);
+        }
     }
-}
+
+left.addEventListener("click", moveleft);
+    function moveright() {
+        if (imageId == 11){
+            imageId = 0;
+            bigimage(imageId);
+        } else {
+            imageId += 1;
+            bigimage(imageId);
+        }
+    }
+
+
+
+
 
 
 
