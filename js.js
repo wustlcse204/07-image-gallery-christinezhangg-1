2c2a0a76ac49ba9dfd159ab22cfe8c3e5c0481d5
christinezhangg <54513295+christinezhangg@users.noreply.github.com>
2022-11-07 16:16:36 -0600

    done

--- js.js
+++ js.js
@@ -7,28 +7,30 @@ var imageId = 0;
 //bigimage was completed with a TA who directed me towards https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp for assistance.
 //Molly okayed everything that I completed here. 
 function bigimage(imgs) {
+    imageId = imgs.getAttribute("id");
+    console.log(imageId);
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
 }
 
 function moveleft() {
-    if (imageId == 1){
-        imageId = 12;
-        bigimage(imageId);
-    } else {
-        imageId -= 1;
-        bigimage(imageId);
+    var src = document.getElementById("expandedImg").getAttribute("src");
+    console.log(src);
+    if (imageId != 1) {
+        imageId--;
+        var img = document.getElementById(imageId);
+        bigimage(img);
     }
 }
 
 function moveright() {
-    if (imageId == 12){
-        imageId = 1;
-        bigimage(imageId);
-    } else {
-        imageId += 1;
-        bigimage(imageId);
+    var src = document.getElementById("expandedImg").getAttribute("src");
+    console.log(src);
+    if (imageId != 12) {
+        imageId++;
+        var img = document.getElementById(imageId);
+        bigimage(img);
     }
 }
 
